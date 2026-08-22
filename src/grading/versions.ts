b4fc107b7bdca4987c8fae53e1a19549dde8485c
Snake020830 <kdjbdi622@gmail.com>
2026-08-22 09:30:27 +0800

    chore: centralize grading workflow version identifiers

---
+++ src/grading/versions.ts
@@ -0,0 +1,3 @@
+export const GRADING_WORKFLOW_VERSION = "shenlun-workflow@0.1.0";
+export const STAGE_PROMPTSET_VERSION = "shenlun-stage-prompts@0.1.0";
+export const BENCHMARK_SCHEMA_VERSION = "shenlun-benchmark@0.2.0";
