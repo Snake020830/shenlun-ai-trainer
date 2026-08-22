ae880f282cb3f1fddeee23a8b4a646d3221affa9
Snake020830 <kdjbdi622@gmail.com>
2026-08-22 14:42:14 +0800

    style: load grouped public exam variant styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles.css";
 import "./extended.css";
 import "./visualPolish.css";
+import "./publicExamVariants.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
