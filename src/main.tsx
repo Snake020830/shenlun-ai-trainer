75e9406e99a0d494da929af00edf9b5f9a35ec45
Snake020830 <kdjbdi622@gmail.com>
2026-08-25 14:58:39 +0800

    style: activate practice layout v3 and migrate material font default

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,14 @@ import "./visualPolish.css";
 import "./publicExamVariants.css";
 import "./publicSourceBatch.css";
 import "./practiceInk.css";
+import "./practiceLayoutV3.css";
 import "./reviewPolish.css";
+
+const MATERIAL_LAYOUT_V3_MIGRATION_KEY = "shenlun:material-layout:v3";
+if (!localStorage.getItem(MATERIAL_LAYOUT_V3_MIGRATION_KEY)) {
+  localStorage.setItem("shenlun:material-font-size:v2", "17");
+  localStorage.setItem(MATERIAL_LAYOUT_V3_MIGRATION_KEY, "1");
+}
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
